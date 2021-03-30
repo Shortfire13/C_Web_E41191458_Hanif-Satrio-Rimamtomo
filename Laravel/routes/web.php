1894ff2a90a05376d424e2178dfe88d0e159a2eb
Hanif Rimamtomo <65529407+Shortfire13@users.noreply.github.com>
2021-03-30 10:18:03 +0700

    laravel

--- Laravel/routes/web.php
+++ Laravel/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ManagementUserController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("/user", [ManagementUserController::class, "index"]);
